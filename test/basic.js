2c5a22036a935a472bc483dbad573c0bae9f19c6
Nauman Ramzan <nomi_ramzan@yahoo.com>
2017-06-17 21:16:46 +0400

    mergeArrays and concatArrays options

--- test/basic.js
+++ test/basic.js
@@ -6,12 +6,13 @@ var fs = require('fs')
 
 test('Run basic test', function (t) {
   t.plan(5)
+  
 	builder.loadTasks({
 		jsFiles:    { /*preLog:'path',*/ src:"./test/src/*.js",     ext:'.js', compress:true, /*save:true,*/ 			dest:"./test/dest/js" , concat:"bundle.js" /* || , concat:{name:"bundle", ext:".js"}*/ },
 		jsFilesEs6: { /*preLog:'path',*/ src:"./test/src/es6/*.js", ext:'.js', harmony:true, compress:true, dest:"./test/dest/js" , concat:"es6-bundle.js" /* || , concat:{name:"bundle", ext:".js"}*/ },
 		cssFiles:   { /*preLog:'path',*/ src:"./test/src/*.css",    ext:'.css', compress:true, /*save:false,*/   dest:"./test/dest/css" , concat:"bundle.css" /* || , concat:{name:"bundle", ext:".js"}*/ },
 		htmlFiles:  { /*preLog:'path',*/ src:"./test/src/*.html",   ext:'.html', compress:true, dest:"./test/dest/html" /* || , concat:{name:"bundle", ext:".js"}*/ },
-		jsonFiles:  { /*preLog:'path',*/ src:"./test/src/*.json",   ext:'.json', compress:true, dest:"./test/dest/json", concat:{name:"bundle", ext:".json"} }
+		jsonFiles:  { /*preLog:'path',*/ src:"./test/src/apidocs/*.apidoc.json",   ext:'.json', /*compress:true,*/ dest:"./test/dest/json", concat:{name:"bundle", ext:".json"} }
 	});
 	builder.runTasks()
 	setTimeout(function(){
